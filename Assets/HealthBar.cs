2db42fff0d98c20c82b11f91560db8b7698903b8
Sahar <ssharbaf-shoar@csub.edu>
2021-03-10 12:02:43 -0800

    Making health bar

---
+++ Assets/HealthBar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider; 
+    
+    public void SetMaxHealth(int health)    //function to show the maximum amount of health 
+    {
+        slider.maxValue = health;
+        slider.value = health; 
+    }
+    public void SetHealth(int health)       //function to set the value of health bar 
+    {
+        slider.value = health;
+    }
+}
