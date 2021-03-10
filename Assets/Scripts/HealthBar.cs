d32cb885a87d886dd6f07746f76b76983c2d2082
Sahar <ssharbaf-shoar@csub.edu>
2021-03-10 14:11:30 -0800

    Color changing health bar

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -6,15 +6,21 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    public Slider slider; 
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill; 
     
     public void SetMaxHealth(int health)    //function to show the maximum amount of health 
     {
         slider.maxValue = health;
-        slider.value = health; 
+        slider.value = health;
+
+        fill.color = gradient.Evaluate(1f);     //to change the color in health bar 
     }
     public void SetHealth(int health)       //function to set the value of health bar 
     {
         slider.value = health;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 }
