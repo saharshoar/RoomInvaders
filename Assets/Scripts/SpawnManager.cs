5091cfbdfbfa7d9c52d2038a30002de3b3b74388
Matthew <ryan1matthew@gmail.com>
2021-03-19 20:29:13 -0700

    Made 2 enemy base prefabs and a very basic spawn/round system
    2 Enemy prefabs, identical except for shouldShoot variable. Round system keeps track of enemies on the map, increases round and spawns more enemies once the enemy count reaches zero. After round 5, ranged enemies spawn. The spawn points are random and are not finalized at all.

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private GameObject enemyRangedPrefab;
+    [SerializeField] private Text enemyText;
+    [SerializeField] private Text roundText;
+
+    public int enemyCount;
+    public int roundNumber = 1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyText.text = "Enemies left: " + enemyCount.ToString();
+        roundText.text = "Round: " + roundNumber.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyCount = FindObjectsOfType<EnemyController>().Length;
+        enemyText.text = "Enemies left: " + enemyCount.ToString();
+        
+
+        if (enemyCount == 0)
+        {
+            roundNumber++;
+            SpawnEnemyWave(roundNumber);
+            roundText.text = "Round: " + roundNumber.ToString();
+
+            if (roundNumber >= 5)
+            {
+                SpawnRangedWave(roundNumber / 2);
+            }
+        }
+    }
+
+    private Vector3 GenerateSpawnPoint()
+    {
+        float spawnPosX = Random.Range(-8f, 8f);
+        float spawnPosZ = Random.Range(-8f, 8f);
+
+        return new Vector3(spawnPosX, 0.25f, spawnPosZ);
+    }
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPoint(), enemyPrefab.transform.rotation);
+        }
+    }
+
+    void SpawnRangedWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyRangedPrefab, GenerateSpawnPoint(), enemyRangedPrefab.transform.rotation);
+        }
+    }
+}
