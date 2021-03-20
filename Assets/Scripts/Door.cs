3ae022315fb23f52a7b68f405d52358401db8a02
Matthew <ryan1matthew@gmail.com>
2021-03-19 19:51:25 -0700

    Added basic door functionality
    Pop up says to press E to open door when player is near. Door "opens" by moving downward into floor. Door decrements points by chosen points in inspector.

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Door : MonoBehaviour
+{
+    public Animator doorAnim;
+    public GameObject contextBox;
+    public Text contextText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        contextBox.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            contextBox.SetActive(true);
+            contextText.text = "Press E to open for 1000 points";
+
+            if (Input.GetKeyDown(KeyCode.E) && PlayerController.instance.currentPoints >= 1000)
+            {
+                PlayerController.instance.currentPoints -= 1000;
+                doorAnim.SetBool("DoorOpened", true);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            contextBox.SetActive(false);
+        }
+    }
+}
