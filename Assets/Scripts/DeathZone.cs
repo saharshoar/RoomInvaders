e60f43238361e11c780578757cc088fdad926ea1
Matthew <ryan1matthew@gmail.com>
2021-04-10 15:02:11 -0700

    Migrated Game Functionality to GameScene
    Changed some inspector values to match with the GameScene

---
+++ Assets/Scripts/DeathZone.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            PlayerController.instance.playerHealth.currentHealth = -5;
+        }
+        else
+        {
+            Destroy(other.gameObject, 0.05f);
+        }
+    }
+}
