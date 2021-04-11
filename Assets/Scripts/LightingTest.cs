5999419e253ce4f2b2674dae84c99d326d90fd42
Matthew <ryan1matthew@gmail.com>
2021-04-11 13:42:55 -0700

    Added powerSwitch to GameScene and changed LightingTest script
    Now the lights and intensity2song turn on when the powerswitch is on. Intensity3song turns on when player reaches round 10. Both songs gradually turn up in volume over about half a second rather than instantly. The lights turn on "slowly" rather than all of them at once.

--- Assets/Scripts/LightingTest.cs
+++ Assets/Scripts/LightingTest.cs
@@ -7,58 +7,67 @@ public class LightingTest : MonoBehaviour
     public GameObject playerLight;
     public List<GameObject> planePointLights;
     public List<Material> emissionLights;
-    private bool powerOn = false;
     private bool flashLightOn = true;
     public AudioSource intensity2Song;
+    public AudioSource intensity3Song;
+
+    private float lightsCounter = 0.5f;
+    private float emissionsCounter = 0.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        RenderSettings.ambientIntensity = 0f;
-        RenderSettings.reflectionIntensity = 0f;
+        intensity2Song.volume = 0f;
+        intensity3Song.volume = 0f;
+
+        foreach (Material emission in emissionLights)
+        {
+            emission.DisableKeyword("_EMISSION");
+        }
+        foreach (GameObject pointLight in planePointLights)
+        {
+            pointLight.SetActive(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        FlashLightToggle();
+
+        MapLightsToggle();
+        MusicStage1Toggle();
+        MusicStage2Toggle();
+        
+    }
+
+    private void MapLightsToggle()
+    {
+        if (PowerSwitch.instance.powerOn)
         {
-            if (!powerOn)
+            foreach (Material emission in emissionLights)
             {
-                //RenderSettings.ambientIntensity = 1f;
-               // RenderSettings.reflectionIntensity = 1f;
-               foreach (Material emission in emissionLights)
+                lightsCounter -= Time.deltaTime;
+                if (lightsCounter <= 0)
                 {
                     emission.EnableKeyword("_EMISSION");
+                    lightsCounter = 0.5f;
                 }
-                foreach (GameObject pointLight in planePointLights)
+            }
+            foreach (GameObject pointLight in planePointLights)
+            {
+                emissionsCounter -= Time.deltaTime;
+                if (emissionsCounter <= 0)
                 {
                     pointLight.SetActive(true);
+                    emissionsCounter = 0.5f;
                 }
-
-                intensity2Song.volume = 0.45f;
-
-                powerOn = true;
-            }
-            else if (powerOn)
-            {
-                // RenderSettings.ambientIntensity = 0f;
-                // RenderSettings.reflectionIntensity = 0f;
-                foreach (Material emission in emissionLights)
-                {
-                    emission.DisableKeyword("_EMISSION");
-                }
-                playerLight.SetActive(true);
-                foreach (GameObject pointLight in planePointLights)
-                {
-                    pointLight.SetActive(false);
-                }
-
-                intensity2Song.volume = 0f;
-
-                powerOn = false;
             }
         }
+    }
+
+    private void FlashLightToggle()
+    {
         if (Input.GetKeyDown(KeyCode.F))
         {
             if (flashLightOn)
@@ -71,6 +80,36 @@ public class LightingTest : MonoBehaviour
                 playerLight.SetActive(true);
                 flashLightOn = true;
             }
+        }
+    }
+
+    private void MusicStage1Toggle()
+    {
+        if (PowerSwitch.instance.powerOn)
+        {
+            if (intensity2Song.volume < 0.45f)
+            {
+                intensity2Song.volume += 0.1f * Time.deltaTime;
+            }
+        }
+        else if (!PowerSwitch.instance.powerOn)
+        {
+            intensity2Song.volume = 0f;
+        }
+    }
+
+    private void MusicStage2Toggle()
+    {
+        if (SpawnManager.instance.roundNumber >= 10)
+        {
+            if (intensity3Song.volume < 0.5f)
+            {
+                intensity3Song.volume += 0.1f * Time.deltaTime;
+            }
+        }
+        else
+        {
+            intensity3Song.volume = 0f;
         }
     }
 }
