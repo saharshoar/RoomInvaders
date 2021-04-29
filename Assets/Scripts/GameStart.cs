24084e0e64e5ed0e10e04bf86a6b5ba866edbc9d
Matthew <ryan1matthew@gmail.com>
2021-04-28 20:00:38 -0700

    Added an instructions screen before control is given to player
    Just some pictures and words to guide the player slightly, requires the player to press E for it to go away and then gives them control.

---
+++ Assets/Scripts/GameStart.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStart : MonoBehaviour
+{
+    private bool gameStarted = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gameStarted)
+        {
+            PlayerController.instance.canMove = false;
+            Time.timeScale = 0f;
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                gameStarted = true;
+            }
+        }
+        else if (gameStarted)
+        {
+            PlayerController.instance.canMove = true;
+            Time.timeScale = 1f;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            gameObject.SetActive(false);
+
+        }
+    }
+}
