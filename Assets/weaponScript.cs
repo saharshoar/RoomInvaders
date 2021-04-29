6e043bdf20ce2246c0b5f5296b7ca5c09f701d8a
sukh <sonu2943singh@gmail.com>
2021-04-28 19:44:15 -0700

    //game weapon changer press 1 or 2 to switch weapons
    game weapon changer
    press 1 or 2 to switch weapons

---
+++ Assets/weaponScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weaponScript : MonoBehaviour
+{
+    int totalWeapons = 1;
+    public int currentWeaponIndex;
+
+    public GameObject[] guns;
+    public GameObject weaponHolder;
+    public GameObject currentGun;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalWeapons = weaponHolder.transform.childCount;
+        guns = new GameObject[totalWeapons];
+
+        for (int i = 0; i < totalWeapons; i++)
+        {
+            guns[i] = weaponHolder.transform.GetChild(i).gameObject;
+            guns[i].SetActive(false);
+        }
+
+        guns[0].SetActive(true);
+        currentGun = guns[0];
+        currentWeaponIndex = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            //next Weapon
+            if(currentWeaponIndex < totalWeapons-1)
+            {
+                guns[currentWeaponIndex].SetActive(false);
+                currentWeaponIndex += 1;
+                guns[currentWeaponIndex].SetActive(true);
+                currentGun = guns[currentWeaponIndex];
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            //previous Weapon
+            if (currentWeaponIndex > 0)
+            {
+                guns[currentWeaponIndex].SetActive(false);
+                currentWeaponIndex -= 1;
+                guns[currentWeaponIndex].SetActive(true);
+                currentGun = guns[currentWeaponIndex];
+            }
+        }
+    }
+}
